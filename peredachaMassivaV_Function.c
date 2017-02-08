aaa434a5e8939729e858f3f52a1d7325d55d9068
artyrkonovalov <ar4iinfo@gmail.com>
2017-02-08 09:44:02 +0300

    Update peredachaMassivaV_Function.c

--- peredachaMassivaV_Function.c
+++ peredachaMassivaV_Function.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <locale.h>
+#define DAYS_COUNT 7
 
 											//Передача массива в функцию
+
+void printArray(int mas[],int count);
+
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-
-
-
-
+	int mas[] = { 123, 584, 458, 987 };
+	int const masCount = sizeof(mas) / sizeof(int);
+	printArray(mas,masCount);
 
 	return 0;
+}
+void printArray(int mas[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		printf("mas[%i] = %i\n",i,mas[i]);
+	}
 }
